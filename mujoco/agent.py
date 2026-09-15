fix: MILESTONE variance had to be reduced, now model gets positive rewards and truly learns

mujoco/agent.py
@@ -43,7 +43,7 @@ class AntAgent():
     def get_action(self, observation):
 
         dist, _ = self.a2c(observation)
-        return dist.sample()
+        return torch.clamp(dist.sample(), -1, 1)
 
     def update_discount(self):
         self.cumulative_discount *= self.discount

mujoco/mlp.py
@@ -14,11 +14,11 @@ class A2C(nn.Module):
 
         self.backbone = nn.Sequential(
             nn.Linear(n_input, 1024),
-            nn.ReLU(),
+            nn.Tanh(),
             nn.Linear(1024, 1024),
-            nn.ReLU(),
+            nn.Tanh(),
             nn.Linear(1024, 1024),
-            nn.ReLU()
+            nn.Tanh()
         )
 
         self.mu_head = nn.Sequential(
@@ -35,7 +35,7 @@ class A2C(nn.Module):
         shared = self.backbone(observation)
 
         mu = self.mu_head(shared)
-        log_std_clamped = torch.clamp(self.log_std, min=-20, max=2)
+        log_std_clamped = torch.clamp(self.log_std, min=-4.6, max=-1.4)
         std = torch.exp(log_std_clamped)
 
         v_value = self.v_head(shared)